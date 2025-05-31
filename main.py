843b007dc2ec82a45eb6714cccd9bec2ff0f818c
BAD MUNDA <139567638+badmunda011@users.noreply.github.com>
2025-05-31 14:57:35 +0530

    Update main.py

--- main.py
+++ main.py
@@ -44,8 +44,8 @@ async def start(m: UpdateNewMessage):
 𝐏𝐋𝐀𝐍'𝐒 : /plans"""
 
     # Check if the user is a member of both channels
-    channel1 = "@mavimods2"
-    channel2 = "@mavibot_support"  # Replace with the actual username of your second channel
+    channel1 = "@teysysyt"
+    channel2 = "@teysysyt"  # Replace with the actual username of your second channel
 
     if not await is_user_on_chat(bot, channel1, m.peer_id) or not await is_user_on_chat(bot, channel2, m.peer_id):
         return await m.reply("𝐏𝐥𝐞𝐚𝐬𝐞 𝐣𝐨𝐢𝐧 @mavimods2 𝐚𝐧𝐝 @mavibot_support 𝐛𝐞𝐟𝐨𝐫𝐞 𝐮𝐬𝐢𝐧𝐠 𝐭𝐡𝐞 𝐛𝐨𝐭.")
